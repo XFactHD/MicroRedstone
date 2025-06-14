58547ad1ecad076386d1bc9b7abde81d80cb8827
XFactHD <xfacthd@gmx.de>
2025-06-14 14:52:13 +0200

    Initial commit

---
+++ src/main/java/io/github/xfacthd/microredstone/MicroRedstone.java
@@ -0,0 +1,16 @@
+package io.github.xfacthd.microredstone;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(MicroRedstone.MOD_ID)
+@SuppressWarnings("UtilityClassWithPublicConstructor")
+public final class MicroRedstone
+{
+    public static final String MOD_ID = "microredstone";
+
+    public MicroRedstone(IEventBus modBus)
+    {
+
+    }
+}
